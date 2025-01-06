36b0e5f79aaa45284d928de09744faf6c4166ea5
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-01-06 16:23:53 -0300

    criação e estilização do banner principal

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/images/banner.png" alt="Banner Principal da página do Organo."/>
+        </header>
+    );
+}
+
+export default Banner;
