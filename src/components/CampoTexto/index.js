bc4f2ec29ac991ffdc0696b19b6c27003d6fb3c1
rnn <145611850+renanfochetto@users.noreply.github.com>
2025-01-07 19:23:59 -0300

    binding de dados e geração do objeto para criação dos cards

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,12 +1,19 @@
 import './CampoTexto.css'
+import { useState } from "react";
 
 const CampoTexto = (props) => {
+
+    const [valor, setValor] = useState('')
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={props.placeholder} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder} />
         </div>
     )
 }
